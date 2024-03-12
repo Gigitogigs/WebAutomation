dc3ba27318d093f12e32397597f327c1ec65ce62
Gigitogigs <ngigemwaniki2004@gmail.com>
2024-03-12 18:45:12 +0300

    Day 1

--- Selenium web automation.py
+++ Selenium web automation.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-
+import time
 options=Options()
 options.add_experimental_option("detach", True)#Prevents auotmatic closure of page
 
@@ -10,8 +10,7 @@ driver= webdriver.Chrome(service=Service(ChromeDriverManager().install()),option
 
 driver.get("https://www.instagram.com/gigito.gigs")#goes to url
 driver.maximize_window()#maximizes window
+time.sleep(3)
 
-    
-
-     
-    
+login_btn = driver.find_element_by_class_name("")
+login_btn.click()#Click on the login button
